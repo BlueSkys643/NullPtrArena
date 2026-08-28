0eb94d27fa51432013ff425353cd2685bb95ddfc
Skyler Home Desktop <shunsinger643@gmail.com>
2026-08-28 20:46:20 +0000

    added csrf protection token to form

--- arena/views.py
+++ arena/views.py
@@ -2,4 +2,10 @@ from django.shortcuts import render
 
 # Create your views here.
 def submit(request):
+    if request.method == "POST":
+        code = request.POST["editor"]
+        language = request.POST["language"]
+        # run code here
+
+        
     return render(request, "arena/submit.html")
